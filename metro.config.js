d73f2d6932458ae61924b669e4570d06804ecba7
archlordroy-codeur <archlordroy@gmail.com>
2026-05-18 19:25:07 +0100

    Fix build error: remove unused shared folder reference in metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,4 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
-const path = require('path');
 
 /**
  * Metro configuration
@@ -7,23 +6,6 @@ const path = require('path');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {
-  watchFolders: [
-    path.resolve(__dirname, '../shared'),
-  ],
-  resolver: {
-    nodeModulesPaths: [
-      path.resolve(__dirname, 'node_modules'),
-    ],
-    extraNodeModules: new Proxy({}, {
-      get: (target, name) => {
-        if (name === '@shared') {
-          return path.resolve(__dirname, '../shared');
-        }
-        return path.resolve(__dirname, 'node_modules', name);
-      },
-    }),
-  },
-};
+const config = {};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
